b0cbac4ae70de316166c7ea2f3769260220e9b19
Enekopb <91752156+Enekopb@users.noreply.github.com>
2023-11-13 22:37:17 +0100

    Update index.php

--- app/index.php
+++ app/index.php
@@ -3,9 +3,9 @@ require 'db_conn.php';
 $usuario = $_POST['usuario'];
 $contra = $_POST['contra'];
 
-$sql ="SELECT * FROM `Erabiltzaileak` WHERE `erabId` = '$erabIz'";
+$sql ="SELECT * FROM usuarios WHERE 'usuario' = '$usuario' and 'contra' = '$contra'";
 $query = mysqli_query($con,$sql);
-$nr = mysqli_num_rows($query); // nr aldagaian 1 erabiltzailea aurkitu bada 0 erabiltzailea ez bada aurkitu
+$nr = mysqli_num_rows($query); 
 $row = mysqli_fetch_array($query);
 
 if(isset($_POST['inicioSesion'])){
@@ -29,16 +29,16 @@ $conn->close();
 
 <div class="content">
 <body background="fondo.jpg">
-    <form name="loginak" class="inputak" method="POST" action="login2.php"> <!-- Se envia al ".php" la info -->
+    <form name="loginak" class="inputak" method="POST" action="index.php"> 
         <table>
             <tr>
                 <td>&nbsp;</td>
-                <td><input id="erabIzena" type="text" name="erabIzena" placeholder="Erabiltzailea Ad: eneko5" required></td>
+                <td><input id="usuario" type="text" name="usuario" placeholder="Usuario Ad: eneko" required></td>
                 <td>&nbsp;</td>
             </tr>
             <tr>
                 <td>&nbsp;</td>
-                <td><input id="pasahitza" type="password" name="pasahitza" placeholder="Pasahitza Ad: JDkF7SH9" required></td>
+                <td><input id="contra" type="password" name="contra" placeholder="Pasahitza Ad: JDkF7SH9" required></td>
                 <td>&nbsp;</td>
             </tr>
             <tr>
@@ -55,11 +55,11 @@ $conn->close();
             <td>&nbsp;</td>
             <td>&nbsp;</td>
             <td>&nbsp;</td>
-            <td>Ez zara erregistratu?</td>
+            <td>¿No te has registrado todavía?</td>
             <td>&nbsp;</td>
             <tr>
                 <td>&nbsp;</td>
-                <td><input id="erregistroBuelta" type="button" name="erregistroBuelta" value="Registrarse" onclick="location.href='registro.php'" /></td>
+                <td><input id="registro" type="button" name="registro" value="Registrarse" onclick="location.href='registro.php'" /></td>
                 <td>&nbsp;</td>
             </tr>
         </tr>
