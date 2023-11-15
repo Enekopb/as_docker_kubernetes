9526543ec83714c42fa1bab5b242ed15bbb2cc65
Enekopb <91752156+Enekopb@users.noreply.github.com>
2023-11-15 21:15:26 +0100

    Update init.sql añadir email

--- db/init.sql
+++ db/init.sql
@@ -4,6 +4,7 @@ use database;
 CREATE TABLE usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
     password VARCHAR(200) NOT NULL
 );
 
