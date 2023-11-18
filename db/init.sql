64cccb3da48c21aaac589dcc3c7343f580737d84
Enekopb <91752156+Enekopb@users.noreply.github.com>
2023-11-18 10:29:02 +0100

    Update init.sql valores añadidos a rutas

--- db/init.sql
+++ db/init.sql
@@ -1,17 +1,17 @@
-CREATE DATABASE database;
-use database;
-
-CREATE TABLE usuarios (
+CREATE TABLE if not exists usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL,
-    password VARCHAR(200) NOT NULL
+    password VARCHAR(200) NOT NULL,
+    UNIQUE KEY (username)
 );
 
-CREATE TABLE rutas (
+CREATE TABLE if not exists rutas (
     id INT AUTO_INCREMENT PRIMARY KEY,
     usuario VARCHAR(50),
     nombre_ruta VARCHAR(100),
     kilometros FLOAT,
+    duracion FLOAT,
+    desnivel FLOAT,
     FOREIGN KEY (usuario) REFERENCES usuarios(username)
 );
