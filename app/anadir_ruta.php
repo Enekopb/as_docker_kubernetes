1da0235f6317c0c779a29b9fafab1df28810b491
Enekopb <91752156+Enekopb@users.noreply.github.com>
2023-11-18 10:32:15 +0100

    Add files via upload

---
+++ app/anadir_ruta.php
@@ -0,0 +1,63 @@
+<?php
+require 'dbkon.php';
+session_start();
+
+$ruta = $_POST['ruta'];
+$km = $_POST['kilometros'];
+$usuario= $_SESSION['usuario'];
+$desnivel=$_POST['desnivel'];
+$duracion=$_POST['duracion'];
+
+if(isset($_POST['enviar'])){
+
+	$sql ="INSERT INTO rutas (usuario, nombre_ruta, kilometros,duracion,desnivel) VALUES ('$usuario','$ruta','$km', '$duracion', '$desnivel')";
+		if ($con->query($sql) === TRUE) {
+	        	header("Location: http://localhost:8080/menu.php"); // Al menu
+			echo "Datos insertados correctamente";
+    		} else {
+        		echo "Error al insertar datos: " . $con->error;
+    		}	
+        }
+
+$con->close();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Rutas propias</title>
+    <link rel="stylesheet" type="text/css" href="styles2.css">
+</head>
+<body>
+    <h1>Bienvenido, <?php echo $_SESSION['usuario']; ?>!</h1>
+    <form name="rutanueva" id="rutanueva" class="rutanueva" action="anadir_ruta.php" method="POST" >
+	  <table>
+            <tr>
+                <td>&nbsp;</td>
+                <td><input class="form-control" id="ruta" type="text" name="ruta" placeholder="Nombre ruta:" required/></td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td><input class="form-control" id="kilometros" type="text" name="kilometros" placeholder="kilometros" required/></td>
+                <td>&nbsp;</td>
+	    </tr>
+	    <tr>
+                <td>&nbsp;</td>
+                <td><input class="form-control" id="duracion" type="text" name="duracion" placeholder="duracion" required/></td>
+                <td>&nbsp;</td>
+            </tr>
+	    <tr>
+                <td>&nbsp;</td>
+                <td><input class="form-control" id="desnivel" type="text" name="desnivel" placeholder="desnivel" required/></td>
+                <td>&nbsp;</td>
+            </tr>
+	    <tr>
+                <td>&nbsp;</td>
+                <td><input id ="enviar" type="submit" name="enviar" value="enviar" ></td>                
+                <td>&nbsp;</td>
+            </tr>
+	  </table>
+	</form>
+</body>
+</html>
---
+++ app/registro.php
@@ -0,0 +1,59 @@
+<?php
+	require 'dbkon.php'; 
+	session_start();
+
+	$nombre = $_POST['nombre'];
+        $email = $_POST['email'];
+        $contra = $_POST['password'];
+
+	if(isset($_POST['registrarse'])){
+		
+		$sql ="INSERT INTO usuarios (username, email, password) VALUES ('$nombre','$email','$contra')";
+		if ($con->query($sql) === TRUE) {
+			$_SESSION['usuario'] = $nombre;
+	        	header("Location: http://localhost:8080/menu.php"); // Al menu
+			echo "Datos insertados correctamente";
+    		} else {
+        		echo "Error al insertar datos: " . $con->error;
+    		}	
+        }
+
+$con->close();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Registro</title>
+</head>
+<link rel="stylesheet" type="text/css" href="styles.css">
+
+<div class="content">
+	<form name="registro" id="registro" class="registro" action="registro.php" method="POST" >
+	  <table>
+            <tr>
+                <td>&nbsp;</td>
+                <td><input class="form-control" id="nombre" type="text" name="nombre" placeholder="Nombre" required/></td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td><input class="form-control" id="email" type="text" name="email" placeholder="email" title="ADB: eperez89@gmail.com" required/></td>
+                <td>&nbsp;</td>
+	    </tr>
+	    <tr>
+                <td>&nbsp;</td>
+                <td><input id="password" type="password" name="password" placeholder="Contraseña" required></td>
+                <td>&nbsp;</td>
+	    </tr>
+	    <tr>
+                <td>&nbsp;</td>
+                <td><input id ="registrarse" type="submit" name="registrarse" value="registrarse" ></td>                
+                <td>&nbsp;</td>
+            </tr>
+	  </table>
+	</form>
+	
+</div>
+</body>
+</html>
