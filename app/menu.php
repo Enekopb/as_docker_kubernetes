c74941bd58b7c53aa27967bf03ec6e883849a7e1
Enekopb <91752156+Enekopb@users.noreply.github.com>
2023-11-18 10:31:21 +0100

    Update menu.php

--- app/menu.php
+++ app/menu.php
@@ -1,22 +1,29 @@
 <?php
+require 'dbkon.php';
 session_start();
 
-if (!isset($_SESSION['usuario'])) {
-    header("Location: index.html");
-}
+
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
     <title>Menú Principal</title>
+    <link rel="stylesheet" type="text/css" href="styles2.css">
 </head>
 <body>
-    <h1>Bienvenido, <?php echo $_SESSION['username']; ?>!</h1>
+    <h1>Bienvenido, <?php echo $_SESSION['usuario']; ?>!</h1>
     <ul>
+	<li><a href="anadir_ruta.php">Añadir nueva ruta</a></li>
         <li><a href="ver_rutas_propias.php">Ver mis rutas</a></li>
-        <li><a href="ver_rutas_amigos.php">Ver rutas de amigos</a></li>
-        <li><a href="anadir_ruta.php">Añadir nueva ruta</a></li>
+        <li><a href="http://localhost:3000">Analisis de datos</a></li>
     </ul>
+     <table>
+            <tr>
+                <td>&nbsp;</td>
+                <td><input id="salir" type="button" name="salir" value="Salir" onclick="location.href='index.php' " /></td>
+                <td>&nbsp;</td>
+            </tr>
+    </table>
 </body>
 </html>
