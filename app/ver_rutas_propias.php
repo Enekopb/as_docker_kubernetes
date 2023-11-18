1228ab338542bcb41ee79ffc2460caa64f6f0784
Enekopb <91752156+Enekopb@users.noreply.github.com>
2023-11-18 10:31:45 +0100

    Update ver_rutas_propias.php

--- app/ver_rutas_propias.php
+++ app/ver_rutas_propias.php
@@ -8,42 +8,38 @@ $usuario = $_SESSION['usuario'];
 <html>
 <head>
     <title>Rutas propias</title>
-    <link rel="stylesheet" type="text/css" href="styles2.css">
+    <link rel="stylesheet" type="text/css" href="styles3.css">
 </head>
 <body>
-    <h1>Bienvenido, <?php echo $_SESSION['username']; ?>!</h1>
+    <h1>Bienvenido, <?php echo $_SESSION['usuario']; ?>!</h1>
     <h2>Tus rutas: </h2>
-    <form name="erabAukerak" class="inputak" action="erabileremu.php" method="POST">
-        <tr>
-                <td>&nbsp;</td>
-                <td><input id="vuelta" type="button" name="vuelta" value="vuelta" title="Volver al menu" onclick="location.href='menu.php'"/></td>
-        </tr>
-    </form>
-<?php
-        $db = new mysqli("db", "admin", "test123#", "database");
-        $sql = "SELECT nombre_ruta, kilometros, duracion, desnivel FROM ruta WHERE usuario='$usuario'";
-        $stmt = $db->prepare($sql);
-        $stmt->execute();
-        $resultSet = $stmt->get_result();
-        $datos = $resultSet->fetch_all();
-        foreach($datos as $dato) {
-?>
-
- <div class="php">
-        <table id = "datosTabla" class = "table-striped table-bordered" style = "width:100%">
-            <thead class = "text-center">   
+<div class="php">
+	<table id = "datosTabla" class = "table-striped table-bordered" style = "width:90%">	
+		<thead class = "text-center">
                         <th>Nombre de la ruta</th>
                         <th>Distancia recorrida</th>
                         <th>Duracion</th>
                         <th>Desnivel</th>
                 </thead>
 
-            <tbody>
+<?php
+	$db = new mysqli("db", "admin", "test123#", "database");
+        $sql = "SELECT * FROM rutas WHERE usuario = '$usuario'";
+	$stmt = $db->prepare($sql);
+	if ($stmt === false) {
+    		die("Error al preparar la consulta: " . $ddbb->error);
+	}
+        $stmt->execute();
+        $resultSet = $stmt->get_result();
+        $datos = $resultSet->fetch_all();
+        foreach($datos as $dato) {
+?>
+           <tbody>
                 <tr>
-                    <td><?php echo $datos[0]?></td>
-                    <td><?php echo $datos[1]?></td>
-                    <td><?php echo $datos[2]?></td>
-                    <td><?php echo $datos[3]?></td>
+                    <td><?php echo $dato[2]?></td>
+                    <td><?php echo $dato[3]?></td>
+                    <td><?php echo $dato[4]?></td>
+                    <td><?php echo $dato[5]?></td>
                 </tr>
 
                 <?php } ?>
@@ -51,6 +47,11 @@ $usuario = $_SESSION['usuario'];
             </tbody>
         </table>
     </div>
-
+    <form name="menu" class="inputak" action="menu.php" method="POST">
+        <tr>
+                <td>&nbsp;</td>
+                <td><input id="vuelta" type="button" name="vuelta" value="vuelta" title="Volver al menu" onclick="location.href='menu.php'"/></td>
+        </tr>
+    </form>
 </body>
 </html>
